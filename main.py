9d9126f16bf6d20391ac0fb085682e7b665980b9
Naresh Darji <160341000+darjinaresh@users.noreply.github.com>
2024-02-24 11:36:52 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,94 +1,57 @@
+
+# TASK:03 - PASSWORD GENERATOR USING TKINTER IN PYTHON
+
 from tkinter import *
 import random
-
-
-comp_score = 0
-player_score = 0
-
-outcomes = {
-    "rock": {"rock": 1, "paper": 0, "scissors": 2},
-    "paper": {"rock": 2, "paper": 1, "scissors": 0},
-    "scissors": {"rock": 0, "paper": 2, "scissors": 1}
-}
-
-
-
-def converted_outcome(number):
-    if number == 1:
-        return "rock"
-    elif number == 2:
-        return "paper"
-    elif number == 3:
-        return "scissors"
-
-
-def outcome_handler(user_choice):
-    global comp_score
-    global player_score
-    random_number = random.randint(1, 3)
-    computer_choice = converted_outcome(random_number)
-    outcome = outcomes[user_choice][computer_choice]
-
-    player_choice_label.config(fg="red", text="Player Choice : " + str(user_choice))
-    computer_choice_label.config(fg="green", text="Computer Choice : " + str(computer_choice))
-
-    if outcome == 2:
-        player_score = player_score + 2
-        player_score_label.config(text="Player : " + str(player_score))
-        outcome_label.config(fg="blue", text="Outcome : Player Won ")
-
-    elif outcome == 0:
-        comp_score = comp_score + 2
-        computer_score_label.config(text="Computer : " + str(comp_score))
-        outcome_label.config(fg="blue", text="Outcome : Computer Won")
-
-    elif outcome == 1:
-        player_score = player_score + 1
-        comp_score = comp_score + 1
-        player_score_label.config(text="Player : " + str(player_score))
-        computer_score_label.config(text="Computer : " + str(comp_score))
-        outcome_label.config(fg="blue", text="Outcome : Draw")
-
-
+import string
 
 root = Tk()
-root.title("ROCK-PAPER-SCISSORS")
 
-Font = ("times new roman", 15, "bold")
+def generator():
+    special_character = string.punctuation
+    numbers = string.digits
+    small_alphabets = string.ascii_lowercase
+    capital_alphabets = string.ascii_uppercase
 
-label1 = Label(root, text="ROCK-PAPER-SCISSORS", font=Font, fg="dark green")
-label1.grid(row=0, sticky=N, pady=10, padx=200)
+    all=special_character+numbers+small_alphabets+capital_alphabets
+    password_length = int(length_Box.get())
 
-label2 = Label(root, text="GAME START NOW....", font=Font, fg="dark green")
-label2.grid(row=1, sticky=N)
+    if choice.get() == 1:
+        password_value.insert(0, random.sample(small_alphabets, password_length))
 
-player_score_label = Label(root, text="Player : 0", font=Font)
-player_score_label.grid(row=2, sticky=W)
+    if choice.get() == 2:
+        password_value.insert(0, random.sample(small_alphabets+capital_alphabets, password_length))
 
-computer_score_label = Label(root, text="Computer : 0", font=Font)
-computer_score_label.grid(row=2, sticky=E)
+    if choice.get() == 3:
+        password_value.insert(0, random.sample(all, password_length))
 
-player_choice_label = Label(root, font=Font)
-player_choice_label.grid(row=3, sticky=W)
+root.geometry("300x400")
+root.config(background="black")
+Font =("times new roman", 15, "bold")
+choice = IntVar()
 
-computer_choice_label = Label(root, font=Font)
-computer_choice_label.grid(row=3, sticky=E)
+root.title("Password Generator")
+label =Label(root, text="Password Generator", font=("times new roman", 20, "bold"), foreground="white", background="black", borderwidth=3)
+label.grid(pady=8)
 
-outcome_label = Label(root, font=Font)
-outcome_label.grid(row=3, sticky=N)
+checkbutton1 =Radiobutton(root,text="weak", font=Font,background="red", variable=choice, value=1, borderwidth=3)
+checkbutton1.grid(pady=5)
 
+checkbutton2 =Radiobutton(root,text="Medium", font=Font,background="light green", variable=choice, value=2, borderwidth=3)
+checkbutton2.grid(pady=5)
 
-button = Button(root, text="Rock", width=15, command=lambda: outcome_handler("rock"))
-button.grid(row=4, sticky=W, padx=5, pady=5)
+checkbutton3 =Radiobutton(root,text="Strong", font=Font,background="dark green", variable=choice, value=3, borderwidth=3)
+checkbutton3.grid(pady=5)
 
-button = Button(root, text="Paper", width=15, command=lambda: outcome_handler("paper"))
-button.grid(row=4, sticky=N, pady=5)
+length_label =Label(root, text="Password length", font=("times new roman", 20, "bold"), foreground="white", background="black", borderwidth=3)
+length_label.grid(pady=5)
 
-button = Button(root, text="Scissors", width=15, command=lambda: outcome_handler("scissors"))
-button.grid(row=4, sticky=E, padx=5,pady=5)
+length_Box = Spinbox(root,from_=4,to=10,width=5,font=("times new roman", 15, "bold"))
+length_Box.grid(pady=5)
 
-button = Button(root, text="© 2024 Naresh. All rights reserved.", font=Font)
-button.grid(pady=17)
+Button(root,text="generate", font=Font, command=generator).grid(pady=5)
 
+password_value = Entry(root, width=30, borderwidth=3)
+password_value.grid(pady=5)
 
 root.mainloop()
